5d471cc0d5f6b086f49758080d7c47501892461e
Artmen_V <viliutich.artem@gmail.com>
2019-11-10 23:32:20 +0700

    draft

--- js/index.js
+++ js/index.js
@@ -382,16 +382,49 @@ json = JSON.stringify(json)
 // }
 // console.log(max_date(["2015/02/01", "2015/02/02", "2015/01/03"]));
 
-function min_date(all_dates) {
-  var min_dt = all_dates[0],
-    min_dtObj = new Date(all_dates[0]);
-  all_dates.forEach(function(dt, index) {
-    if (new Date(dt) < min_dtObj) {
-      min_dt = dt;
-      min_dtObj = new Date(dt);
-    }
-  });
-  debugger;
-  return min_dt;
-}
-console.log(min_date(["2015/02/01", "2015/02/02", "2015/01/03"]));
+// function min_date(all_dates) {
+//   var min_dt = all_dates[0],
+//     min_dtObj = new Date(all_dates[0]);
+//   all_dates.forEach(function(dt, index) {
+//     if (new Date(dt) < min_dtObj) {
+//       min_dt = dt;
+//       min_dtObj = new Date(dt);
+//     }
+//   });
+//   debugger;
+//   return min_dt;
+// }
+// console.log(min_date(["2015/02/01", "2015/02/02", "2015/01/03"]));
+
+// function timeConvert(n) {
+//   var num = n;
+//   var hours = num / 60;
+//   var rhours = Math.floor(hours);
+//   var minutes = (hours - rhours) * 60;
+//   var rminutes = Math.round(minutes);
+//   return (
+//     num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s)."
+//   );
+// }
+
+// console.log(timeConvert(200));
+
+// var is_array = function(input) {
+//   if (toString.call(input) === "[object Array]") return true;
+//   return false;
+// };
+// console.log(is_array("w3resource"));
+// console.log(is_array([1, 2, 4, 0]));
+
+var first = function(array, n) {
+  if (array == null) return void 0;
+  if (n == null) return array[0];
+  if (n < 0) return [];
+  return array.slice(0, n);
+};
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([], 3));
+console.log(first([7, 9, 0, -2], 3));
+console.log(first([7, 9, 0, -2], 6));
+console.log(first([7, 9, 0, -2], -3));
