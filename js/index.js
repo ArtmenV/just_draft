00b8ce5eca1bb39d6c7934de248662680fe5012b
Artmen_V <viliutich.artem@gmail.com>
2019-11-24 15:18:30 +0700

    work_today

--- js/index.js
+++ js/index.js
@@ -1375,17 +1375,33 @@
 
 // console.log(count);
 
-const data = [
-  { a: "happy", b: "robin", c: ["blue", "green"] },
-  { a: "tired", b: "panther", c: ["green", "black", "orange", "blue"] },
-  { a: "sad", b: "goldfish", c: ["green", "red"] }
-];
+// const data = [
+//   { a: "happy", b: "robin", c: ["blue", "green"] },
+//   { a: "tired", b: "panther", c: ["green", "black", "orange", "blue"] },
+//   { a: "sad", b: "goldfish", c: ["green", "red"] }
+// ];
 
-const colors = data.reduce((total, amount) => {
-  amount.c.forEach(color => {
-    total.push(color);
-  });
-  return total;
-}, []);
+// const colors = data.reduce((total, amount) => {
+//   amount.c.forEach(color => {
+//     total.push(color);
+//   });
+//   return total;
+// }, []);
 
-console.log(colors);
+// console.log(colors);
+// const users = [
+//   { id: 11, name: "Adam", age: 23, group: "editor" },
+//   { id: 47, name: "John", age: 28, group: "admin" },
+//   { id: 85, name: "William", age: 34, group: "editor" },
+//   { id: 97, name: "Oliver", age: 28, group: "admin" }
+// ];
+
+// let res = users.filter(it => new RegExp("oli", "i").test(it.name));
+
+// console.log(res);
+
+var square = [1, 2, 3, 4, 5].map(function(value) {
+  return Math.pow(value, 2);
+});
+
+console.log(square);
