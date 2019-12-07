aee5e1d7f96f6472c1aa86f0fb16354815ca05ee
Artmen_V <viliutich.artem@gmail.com>
2019-12-07 20:23:45 +0700

    :draft

--- js/index.js
+++ js/index.js
@@ -1773,13 +1773,39 @@
 //   }
 // });
 
-function makeUser() {
-  return {
-    name: "Джон",
-    ref: this
-  };
+// function makeUser() {
+//   return {
+//     name: "Джон",
+//     ref: this
+//   };
+// }
+
+// let user = makeUser();
+
+// alert(user.ref.name);
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printList(list) {
+  let tmp = list;
+
+  while (tmp) {
+    alert(tmp.value);
+    tmp = tmp.next;
+  }
+
 }
 
-let user = makeUser();
-
-alert(user.ref.name);
+printList(list);
