1b8bd218f978cf8fe5d10ba67d51922c692b0123
Artmen_V <viliutich.artem@gmail.com>
2019-12-14 20:24:50 +0700

    ststss

--- js/index.js
+++ js/index.js
@@ -1811,13 +1811,13 @@
 // printList(list);
 
 function askPassword(ok, fail) {
-  let password = prompt("Password?", '');
+  let password = prompt("Password?", "");
   if (password == "rockstar") ok();
   else fail();
 }
 
 let user = {
-  name: 'Вася',
+  name: "Вася",
 
   loginOk() {
     alert(`${this.name} logged in`);
@@ -1825,8 +1825,37 @@ let user = {
 
   loginFail() {
     alert(`${this.name} failed to log in`);
-  },
-
+  }
 };
 
 askPassword(user.loginOk, user.loginFail);
+
+let range = {
+  from: 1,
+  to: 5,
+
+  // for..of вызывает этот метод один раз в самом начале
+  [Symbol.iterator]() {
+    // ...возвращает объект-итератор:
+    // далее for..of работает только с этим объектом, запрашивая следующее значение вызовом next()
+    return {
+      current: this.from,
+      last: this.to,
+
+      // next() вызывается на каждой итерации цикла for..of
+      next() {
+        // (2)
+        // должен возвращать значение в виде объекта {done:.., value :...}
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+};
+
+for (let value of range) {
+  alert(value); // 1, потом 2, потом 3, потом 4, потом 5
+}
